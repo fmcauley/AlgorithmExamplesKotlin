6576404c19472d4f57c30697253526ac21fd7fba
Frank McAuley <frank.mcauley@gmail.com>
2020-11-17 21:05:45 -0500

    FM - Adding unit test, JUnit 4 and a simple Binary Search

--- src/main/kotlin/BinarySearch.kt
+++ src/main/kotlin/BinarySearch.kt
@@ -1,2 +1,24 @@
 class BinarySearch {
+    fun searchFor(value: Int, given: Collection<Int>) : Boolean {
+
+        var start = given.first()
+        var end = given.last()
+
+       while (start <= end) {
+           val mid = (start + end) / 2
+           if (value == mid) {
+                return  true
+           }
+
+           if (mid > value) {
+               end = mid - 1
+           }
+
+           if (mid < value) {
+               start = mid + 1
+           }
+       }
+
+        return false
+    }
 }
--- src/test/kotlin/BinarySearchTest.kt
+++ src/test/kotlin/BinarySearchTest.kt
@@ -6,4 +6,24 @@ class BinarySearchTest {
         val bs = BinarySearch()
         assertNotNull(bs)
     }
+
+    @Test
+    fun testThatBinarySearchCanFindAGivenNumber() {
+        val bs = BinarySearch()
+        val values = arrayListOf<Int>(1,2,3,4,5)
+        val searchValue = 2
+        val output = bs.searchFor(searchValue,values)
+
+        assertTrue(output)
+    }
+
+    @Test
+    fun testThatBinarySearchWillReturnFalseWhenAValueIsNotFound() {
+        val bs = BinarySearch()
+        val values = arrayListOf<Int>(1,2,3,4)
+        val searchValue = 5
+        val output = bs.searchFor(searchValue,values)
+
+        assertFalse(output)
+    }
 }
