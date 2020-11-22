aca0094fde257c10b7721eb1ba995801e311e2f2
Frank McAuley <frank.mcauley@gmail.com>
2020-11-22 12:47:37 -0500

    Array Shuffle
    FM - Shuffle the values in an array using Fisher - Yates
    
    Signed-off-by: Frank McAuley <frank.mcauley@gmail.com>

---
+++ src/test/kotlin/ArrayShuffleTest.kt
@@ -0,0 +1,11 @@
+import org.junit.Assert.*
+import org.junit.Test
+
+class ArrayShuffleTest {
+
+    @Test
+    fun arrayShuffleShouldNotBeNull() {
+        val ash = ArrayShuffle()
+        assertNotNull(ash)
+    }
+}
